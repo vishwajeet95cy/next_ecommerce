758e5031467b3f9a27b961cda446a5be4eccf280
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-09-25 22:32:59 +0530

    feat: Footer Added List Page Started

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ListPage = () => {
-  return <div className="">ListPage</div>;
+  return <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64"></div>;
 };
 
 export default ListPage;
