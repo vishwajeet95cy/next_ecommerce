50da81e262dd06a56b77f92871e2fd96dbd875fc
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-09-25 20:07:46 +0530

    feat: Home Page Category List UI Done Product Same List Added Footer Started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,18 @@ const HomePage = () => {
     <div className="">
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-2xl mb-24">Featured Products</h1>
+        <h1 className="text-2xl mb-12">Featured Products</h1>
         <ProductList />
       </div>
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-2xl mb-24">Categories</h1>
+      <div className="mt-24">
+        <h1 className="text-2xl mb-12 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+          Categories
+        </h1>
         <CategoryList />
+      </div>
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl mb-12">New Products</h1>
+        <ProductList />
       </div>
     </div>
   );
