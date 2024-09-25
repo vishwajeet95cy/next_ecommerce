c3c918135cf6bae66f97f46a2875e19ff829ebfd
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-09-25 18:01:56 +0530

    feat: Navbar Added Cart Model Started

---
+++ src/components/CartModel.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const CartModel = () => {
+  return <div>CartModel</div>;
+};
+
+export default CartModel;
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -25,6 +25,7 @@ const Menu = () => {
           <Link href="/">Shop</Link>
           <Link href="/">Deals</Link>
           <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
           <Link href="/">Logout</Link>
           <Link href="/">Cart(1)</Link>
         </div>
